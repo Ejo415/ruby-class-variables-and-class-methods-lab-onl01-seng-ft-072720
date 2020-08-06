f881326a19d3448adf93f1e01cb654055dd0f465
Eugene O'Neil <ejo7220@gmail.com>
2020-08-06 18:54:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,13 +47,13 @@ end
 def self.artist_count
  artist_count = {}
  @artists.each do |artist|
-   if genre_count[genre]
-     genre_count[genre] += 1 
+   if artist_count[artist]
+     artist_count[artist] += 1 
    else
-     genre_count[genre] = 1 
+     artist_count[artist] = 1 
    end
   end
-genre_count
+artist_count
 end
 
 
