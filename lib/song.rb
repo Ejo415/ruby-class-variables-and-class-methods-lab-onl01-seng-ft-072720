5e9ab83a560f9159ffb5b3cfb7e547b16a9cd73b
Eugene O'Neil <ejo7220@gmail.com>
2020-08-06 17:41:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   @@count = 0 
   @@artists = 0
   @@genres = []
+ 
  def  initialize(name,artist,genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count += 1 
+  @@genres << 
 end
 
 attr_accessor :name, :artist, :genre
