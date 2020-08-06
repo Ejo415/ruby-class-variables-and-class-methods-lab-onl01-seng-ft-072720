8f6513d8e06688b5c9737a278e4cb17d59a8b7f3
Eugene O'Neil <ejo7220@gmail.com>
2020-08-06 17:34:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song 
   @@count
- def  initialize(name, artist, genre)
-  name = @name
-  artist = @artist
-  genre = @artist
+ def  initialize(name,artist,genre)
+  @name = name
+  @artist = artist
+  @genre = artist
   @@count += 1 
 end
 
